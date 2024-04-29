dbbd3ddd6fc38f5b8252a90545df478da6728ef2
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-04-29 23:14:32 +0700

    add ast package

--- repl/repl.go
+++ repl/repl.go
@@ -10,19 +10,20 @@ import (
 
 const PROMPT = ">>"
 
-func Start(in io.Reader, out io.WriteCloser){
-  scanner := bufio.NewScanner(in)
-  for {
-    fmt.Println(PROMPT)
-    scanned := scanner.Scan()
-    if !scanned{
-      return
-    }
-    line := scanner.Text()
-    l := lexer.New(line)
-    for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken(){
-      fmt.Printf("%+v\n", tok)
-    }
-  }
-  
+func Start(in io.Reader, out io.WriteCloser) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		fmt.Println(scanned)
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+
 }
