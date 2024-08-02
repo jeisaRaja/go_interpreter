c1ae26635fd0f7765a0a6f322cfab59b7ed23894
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-08-03 02:01:08 +0700

    test: evaluator package

--- object/object.go
+++ object/object.go
@@ -6,6 +6,8 @@ type ObjectType string
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type Object interface {
@@ -20,6 +22,29 @@ type Integer struct {
 func (in *Integer) Inspect() string {
 	return fmt.Sprintf("%d", in.Value)
 }
+
 func (in *Integer) Type() ObjectType {
 	return INTEGER_OBJ
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+
+func (n *Null) Inspect() string {
+	return fmt.Sprintf("null")
 }
